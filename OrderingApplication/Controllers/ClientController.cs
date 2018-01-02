770be23889558995a0495b1fd92c036f0de04939
Owain Williams <owain@andersdigital.com>
2018-01-02 15:11:29 +0000

    Did some course at work

--- OrderingApplication/Controllers/ClientController.cs
+++ OrderingApplication/Controllers/ClientController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using OrderingApplication.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,6 +15,13 @@ namespace OrderingApplication.Controllers
         public IActionResult NewClient()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewClient(Client newClient)
+        {
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
